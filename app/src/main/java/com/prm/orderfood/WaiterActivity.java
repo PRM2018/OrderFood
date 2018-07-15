6d927a273e8a21539c9787fc5e5d37d3ae485a8b
DATDT-TT-D1\BadassPsycho <qwerty.lynkaa@gmail.com>
2018-07-15 13:39:03 +0700

    Add basic design for activity_waiter.xml, add menu/menu_main.xml to WaiterActivity.java.

--- app/src/main/java/com/prm/orderfood/WaiterActivity.java
+++ app/src/main/java/com/prm/orderfood/WaiterActivity.java
@@ -1,7 +1,10 @@
 package com.prm.orderfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class WaiterActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class WaiterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_profile) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            // Push data into intent to ProfileActivity
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
